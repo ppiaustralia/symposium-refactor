a034dc6a6842bc9ee128090fabc79f122f7612e1
rickyfelix19 <rickyfelix19@outlook.com>
2023-01-09 22:11:34 +1100

    add KIPI

--- frontend/pages/KIPI/kipi.js
+++ frontend/pages/KIPI/kipi.js
@@ -2,7 +2,63 @@ import React from 'react'
 
 export default function kipi() {
   return (
-    <div>kipi</div>
+    <div>
+      
+      <section>
+
+      <div>
+      <h1>Konferensi International Pelajar Indonesia</h1>
+      <h3>2022</h3>
+      <p>KIPI is a biennial academic conference hosted by PPI Austalia and has for years provided Indonesian students and researchers, particularly those studying in Australia, with a platform to share their research and discuss burning issues facing Indonesia.</p>
+      </div>
+      </section>
+      
+      <section>
+        <div>
+          <h2>Call for Abstracts and Posters</h2>
+          <p>
+            To participate in KIPI as a presenter, you need to register and submit an abstract —a short summary of research paper on the topic you have chosen. All abstracts will be reviewed and presented at the KIPI 2022 Conference. However, if the prospective presenters could not join the live presentation or want to submit a poster, they are required to submit an academic poster.
+          </p>
+          <button>READ MORE</button>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <h2>What Are The Requirements?</h2>
+          <p>KIPI is open to all Indonesian students who are studying within and outside of Australia. The primary requirement is to be an Indonesian citizen and to be enrolled in an institution of higher learning (undergraduate / postgraduate / vocational students) at the time of the abstract deadline. You can find our template and guideline below</p>
+          <div>
+            <button>READ MORE</button>
+            <button>POSTER GUIDELINES</button>
+          </div>
+      </section>
+
+      <section>
+        <div>
+          <p>timeline</p>
+        </div>
+      </section>
+
+      <section>
+        <div>
+        {/* <image></image> */}
+        <h4>Registration is Free!</h4>
+          <div>
+            <button>AS PRESENTER</button>
+            <button>AS PARTICIPANT</button>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <p>Got any questions? Click the button below to read through our Frequently Asked Questions</p>
+          <button>OPEN KIPI FAQ</button>
+        </div>
+      </section>
+
+        </div>
+    </div>
   )
 }
 
