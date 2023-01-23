895626ad02fb2c29250cb0d41562554401cb0ebc
rickyfelix19 <rickyfelix19@outlook.com>
2023-01-23 14:37:17 +1100

    delete Career Fest folder

--- frontend/pages/Career Fest/careerFest.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function careerFest() {
-  return (
-    <div>careerFest</div>
-  )
-}
--- frontend/pages/Career Fest/careerFestCompanies.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function careerFestCompanies() {
-  return (
-    <div>careerFestCompanies</div>
-  )
-}
